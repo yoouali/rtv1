60fae1b436f29050e9fb94a758cc78c98bb95ec9
Ayoub Agoumi <ayagoumi@e2r3p15.1337.ma>
2021-03-01 18:36:51 +0100

    camera

--- srcs/raycast/raycast.c
+++ srcs/raycast/raycast.c
@@ -178,13 +178,8 @@ int shadow(t_ray *r, t_hsno *hsno, t_light *l, t_object *objects)
     t_vec3 old_hit;
     t_object *tmp;
 
-    // Problem is here
     shadow_ray.origin = l->pos;
-    // if (dot(hsno->l_dir, hsno->l_dir) < 0)
-        // shadow_ray.dir = mult2(-1, hsno->l_dir);
-    // else if (dot(hsno->l_dir, hs/no->l_dir) > 0)
     shadow_ray.dir = hsno->l_dir;
-    // End of Problem
     old_hit = plus(r->origin, mult2(hsno->hit, r->dir));
     dis_light = sqrt(dot(minus(l->pos, old_hit), minus(l->pos, old_hit)));
     tmp = objects;
@@ -193,7 +188,7 @@ int shadow(t_ray *r, t_hsno *hsno, t_light *l, t_object *objects)
         if (tmp != hsno->object)
         {
             hit = calc_intersect(shadow_ray, tmp);
-            if (hit >= 0.0001 && (dis_light > hit))
+            if (hit >= 0.0001 && (dis_light > hit && dis_light - hit > 0.0001))
                 return (0);
         }
         tmp = tmp->next;
@@ -208,61 +203,38 @@ int ft_get_full_color(t_hsno *hsno, t_light *light, t_scene *scene, t_ray *ray)
     int g;
     int b;
     int sh;
-
-    hsno->ambcoef = 0.1;
-    hsno->specoef = 0;
-    hsno->difcoef = 0;
-    hsno->light = light;
-    while (hsno->light)
-    {
-        hsno->difcoef += ft_get_diffuse(hsno, hsno->light);
-        hsno->specoef += ft_get_specular(hsno, hsno->light);
-        hsno->light = hsno->light->next;
-    }
-    sh = shadow(ray, hsno, scene->light, scene->object);
-    r = hsno->object->color.x * hsno->ambcoef + (int)(hsno->object->color.x * hsno->difcoef + 255 * hsno->specoef) * sh;
-    if (r > 255)
-        r = 255;
-    g = hsno->object->color.y * hsno->ambcoef + (int)(hsno->object->color.y * hsno->difcoef + 255 * hsno->specoef) * sh;
-    if (g > 255)
-        g = 255;
-    b = hsno->object->color.z * hsno->ambcoef + (int)(hsno->object->color.z * hsno->difcoef + 255 * hsno->specoef) * sh;
-    if (b > 255)
-        b = 255;
-    color = (r << 16 | g << 8 | b);
-    return (color);
-}
-
-int ft_get_full_color2(t_hsno *hsno, t_light *light, t_scene *scene, t_ray *ray)
-{
-    int color;
-    int r;
-    int g;
-    int b;
-    int sh;
     double intensity;
 
     hsno->ambcoef = 0.1;
     hsno->specoef = 0;
     hsno->difcoef = 0;
     hsno->light = light;
-    while (hsno->light)
-    {
-        sh = shadow(ray, hsno, hsno->light, scene->object);
-        intensity = map(hsno->light->intensity, 100, 0, 1);
-        hsno->difcoef += ft_get_diffuse(hsno, hsno->light) * sh * intensity;
-        hsno->specoef += ft_get_specular(hsno, hsno->light) * sh * intensity;
-        hsno->light = hsno->light->next;
-    }
-    r = hsno->object->color.x * hsno->ambcoef + (int)(hsno->object->color.x * hsno->difcoef + 255 * hsno->specoef);
+    r = hsno->object->color.x * hsno->ambcoef;
     if (r > 255)
         r = 255;
-    g = hsno->object->color.y * hsno->ambcoef + (int)(hsno->object->color.y * hsno->difcoef + 255 * hsno->specoef);
+    g = hsno->object->color.y * hsno->ambcoef;
     if (g > 255)
         g = 255;
-    b = hsno->object->color.z * hsno->ambcoef + (int)(hsno->object->color.z * hsno->difcoef + 255 * hsno->specoef);
+    b = hsno->object->color.z * hsno->ambcoef;
     if (b > 255)
         b = 255;
+    while (hsno->light)
+    {
+        intensity = map(hsno->light->intensity, 100, 0, 1);
+        hsno->difcoef = ft_get_diffuse(hsno, hsno->light) * intensity;
+        hsno->specoef = ft_get_specular(hsno, hsno->light) * intensity;
+        sh = shadow(ray, hsno, hsno->light, scene->object);
+        r += (int)(hsno->object->color.x * hsno->difcoef + hsno->light->color.x * hsno->specoef) * sh;
+        if (r > 255)
+            r = 255;
+        g += (int)(hsno->object->color.y * hsno->difcoef + hsno->light->color.y * hsno->specoef) * sh;
+        if (g > 255)
+            g = 255;
+        b += (int)(hsno->object->color.z * hsno->difcoef + hsno->light->color.z * hsno->specoef) * sh;
+        if (b > 255)
+            b = 255;
+        hsno->light = hsno->light->next;
+    }
     color = (r << 16 | g << 8 | b);
     return (color);
 }
@@ -275,29 +247,30 @@ void ft_cast_rays(t_rtv1 *rtv, t_scene *scene)
     double u, v;
     t_hsno hsno;
 
-    r.origin.y = 0;
-    r.origin.y = 0;
-    r.origin.z = -1;
+    r.origin.x = scene->camera->pos.x + 0.0005;
+    r.origin.y = scene->camera->pos.y;
+    r.origin.z = scene->camera->pos.z;
     i = 0;
+    t_vec3 wv = normalize(minus(scene->camera->dir, scene->camera->pos));
+    t_vec3 uv = cross((t_vec3){0.005, 1, 0}, wv);
+    t_vec3 vv = cross(wv, uv);
+    double h = tanf((scene->camera->zoom * M_PI / 180.0) / 2);
+    double w = (double)(WIDTH / HEIGHT) * h;
     while (i < WIDTH)
     {
         j = 0;
         while (j < HEIGHT)
         {
             hsno.object = NULL;
-            u = map(i, WIDTH, -1, 1);
-            v = map(j, HEIGHT, 1, -1);
-            r.dir.x = u;
-            r.dir.y = v;
-            r.dir.z = 1;
+            u = map(i, WIDTH, -1, 1) * w;
+            v = map(j, HEIGHT, 1, -1) * h;
+            r.dir = plus(wv, plus(mult2(v, vv), mult2(u, uv)));
             r.dir = normalize(r.dir);
             hsno.object = raycast_object(r, &hsno, scene);
             if (hsno.object)
             {
                 if (scene->light)
-                {
-                    rtv->data[(WIDTH * j) + i] = ft_get_full_color2(&hsno, scene->light, scene, &r);
-                }
+                    rtv->data[(WIDTH * j) + i] = ft_get_full_color(&hsno, scene->light, scene, &r);
                 else
                     rtv->data[(WIDTH * j) + i] = ((int)(hsno.object->color.x) << 16 | (int)(hsno.object->color.y) << 8 | (int)(hsno.object->color.z));
             }
